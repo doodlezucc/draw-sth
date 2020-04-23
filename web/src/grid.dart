e2df0d327d64e7bbe587f1ba49ae326072f6cc2c
doodlezucc <electerminator@gmail.com>
2020-04-23 18:04:57 +0000

    draggable grid

--- web/src/grid.dart
+++ web/src/grid.dart
@@ -20,26 +20,46 @@ class Grid {
     project.redraw();
   }
 
-  Point<num> _position = Point(50, 50);
+  Point<num> _position;
   Point<num> get position => _position;
   set position(Point<num> position) {
     _position = position;
-    project.redraw();
+    el.style.left = position.x.toString() + 'px';
+    el.style.top = position.y.toString() + 'px';
   }
 
-  Point<num> _size = Point(100, 100);
+  Point<num> _size;
   Point<num> get size => _size;
   set size(Point<num> size) {
     _size = size;
-    // will redraw twice if position is also changed
-    project.redraw();
+    el.style.width = size.x.toString() + 'px';
+    el.style.height = size.y.toString() + 'px';
   }
 
+  static const dragSensitivity = 10;
+
   Grid(this.project) {
+    position = Point(50, 50);
+    size = Point(100, 100);
+
     el.onMouseDown.listen((e) {
-      var off1 = e.client;
+      var mouse1 = e.client;
+      var pos1 = position;
+      var drag = false;
       var subMove = document.onMouseMove.listen((e) {
-        position += e.client - off1;
+        if (e.movement.magnitude == 0) return;
+
+        var newPos = pos1 + e.client - mouse1;
+        if (!drag &&
+            position.squaredDistanceTo(newPos) >=
+                dragSensitivity * dragSensitivity) {
+          drag = true;
+        }
+
+        if (drag) {
+          position = newPos;
+          project.redraw();
+        }
       });
 
       var subUp;
@@ -51,12 +71,27 @@ class Grid {
   }
 
   void drawOn(CanvasRenderingContext2D ctx) {
-    ctx.strokeStyle = '#fff';
+    ctx.fillStyle = '#fff6';
 
-    for (var i = 0; i <= divisions.x; i++) {
-      var x = position.x + size.x * (i / divisions.x);
+    ctx.fillRect(position.x, position.y, size.x, size.y);
+
+    ctx.strokeStyle = '#fff';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+
+    var lines = Point<int>(divisions.x + 1, divisions.y + 1);
+
+    for (var i = 0; i <= lines.x; i++) {
+      var x = position.x + size.x * (i / lines.x) - 0.5;
       ctx.moveTo(x, position.y);
       ctx.lineTo(x, position.y + size.y);
     }
+    for (var i = 0; i <= lines.y; i++) {
+      var y = position.y + size.y * (i / lines.y) - 0.5;
+      ctx.moveTo(position.x, y);
+      ctx.lineTo(position.x + size.x, y);
+    }
+    ctx.closePath();
+    ctx.stroke();
   }
 }
